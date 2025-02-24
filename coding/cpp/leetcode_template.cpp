f1e35a57354b807d747263a8ba7f9a9b428ff651
Vinod Gomathi nayagam <vinod.gomathinayagam@gmail.com>
2025-02-23 16:05:26 -0800

    move folders

---
+++ coding/cpp/leetcode_template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+	public:
+		void helloworld() {
+			cout << "hello world" << endl;
+		}
+};
+
+int main() {
+	Solution sol;
+	sol.helloworld();
+	return 0;
+}
